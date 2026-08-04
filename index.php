2a1cf2105e212f5995bb4ae5df1fe9b68f437711
Charles Severance <csev@umich.edu>
2026-08-04 17:38:31 -0400

    Add analytics

--- index.php
+++ index.php
@@ -8,6 +8,14 @@
   <title>Building Web Applications in Django — CLOC</title>
   <link rel="icon" href="favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="styles.css">
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-CMHFYWME8L"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-CMHFYWME8L');
+  </script>
 </head>
 <body>
   <header>
